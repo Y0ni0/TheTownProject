6e7bad7957c5140c6af3de46778bf8fabc642a38
Y0ni0 <eshcolsam@gmail.com>
2022-08-05 13:17:37 +0300

    synch asynch 2

--- script2.js
+++ script2.js
@@ -1,12 +1,20 @@
-const second = () => {
-   setTimeout( () =>{
-    console.log('Async Hey there');
-   }, 2000);
-}
+// const second = () => {
+//    setTimeout( () =>{
+//     console.log('Async Hey there');
+//    }, 2000);
+// }
 
-const first = () => {
-    console.log('Hey there');
-    second();
-    console.log('The end')
-}
-first();
+// const first = () => {
+//     console.log('Hey there');
+//     second();
+//     console.log('The end')
+// }
+// first();
+
+    function getrecipe(){
+        setTimeout( () =>{
+            const recipeID = [189, 236, 873, 382];
+            console.log(recipeID);
+        }, 1500)
+    }
+    getrecipe();
